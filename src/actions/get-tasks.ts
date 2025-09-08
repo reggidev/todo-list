3b1ca9574e581c789903ece07eab1c7813a761dd
reggidev <reggidev@gmail.com>
2025-09-08 15:15:18 -0300

    feat: add get tasks server action

---
+++ src/actions/get-tasks.ts
@@ -0,0 +1,12 @@
+'use server'
+
+import { prisma } from '@/utils/prisma'
+
+export const getTasks = async () => {
+  const tasks = await prisma.tasks.findMany()
+
+  if (!tasks) return
+
+  console.log(tasks)
+  return tasks
+}
