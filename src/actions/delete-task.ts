99958d0206838797cccdfcb9cf3c95624a4c87a4
reggidev <reggidev@gmail.com>
2025-09-09 18:00:47 -0300

    feat: add delte task server action

---
+++ src/actions/delete-task.ts
@@ -0,0 +1,21 @@
+'use server'
+
+import { prisma } from '@/utils/prisma'
+
+export const deleteTask = async (id: string) => {
+  try {
+    if (!id) return
+
+    const deletedTask = await prisma.tasks.delete({
+      where: {
+        id,
+      },
+    })
+
+    if (!deletedTask) return
+
+    return deletedTask
+  } catch (error) {
+    console.log(error)
+  }
+}
