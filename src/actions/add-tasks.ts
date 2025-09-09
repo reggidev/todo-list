82c374f75517deb901e7f174df2db8cd59c53208
reggidev <reggidev@gmail.com>
2025-09-09 18:00:39 -0300

    feat: add add task server action

---
+++ src/actions/add-tasks.ts
@@ -0,0 +1,22 @@
+'use server'
+
+import { prisma } from '@/utils/prisma'
+
+export const NewTask = async (task: string) => {
+  try {
+    if (!task) return
+
+    const newTask = await prisma.tasks.create({
+      data: {
+        task,
+        done: false,
+      },
+    })
+
+    if (!newTask) return
+
+    return newTask
+  } catch (error) {
+    console.log(error)
+  }
+}
