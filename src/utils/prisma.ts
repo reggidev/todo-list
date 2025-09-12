7ac415e819ced19b644a8fde84fd21782697cd2b
reggidev <reggidev@gmail.com>
2025-09-12 16:16:51 -0300

    chore: prisma client

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,22 +1,3 @@
-/* import { PrismaClient } from '@/generated/prisma'
+import { PrismaClient } from '@/generated/prisma'
 
 export const prisma = new PrismaClient()
- */
-
-import { PrismaClient } from '@prisma/client'
-
-declare global {
-  var cachedPrisma: PrismaClient
-}
-
-let prisma: PrismaClient
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient()
-} else {
-  if (!global.cachedPrisma) {
-    global.cachedPrisma = new PrismaClient()
-  }
-  prisma = global.cachedPrisma
-}
-
-export const db = prisma
